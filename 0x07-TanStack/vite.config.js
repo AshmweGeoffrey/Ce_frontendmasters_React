a2663c03eaf0a27e69ee335b953f31f95565c69a
AshmweGeoffrey <ashimwegeoffrey@gmail.com>
2025-02-12 17:37:53 +0200

    Fix: React TanStack

--- 0x07-TanStack/vite.config.js
+++ 0x07-TanStack/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouteVite } from '@tanstack/router-plugin/vite';
-
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -16,5 +15,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [TanStackRouteVite(),react()],
+  plugins: [TanStackRouterVite(),react()],
 });
