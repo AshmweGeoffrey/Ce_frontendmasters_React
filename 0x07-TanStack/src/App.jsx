b1dfe3accdd8fb40c5854681ead07c720dd6b674
AshmweGeoffrey <ashimwegeoffrey@gmail.com>
2025-02-12 15:50:36 +0200

    Add: TanStack

--- 0x07-TanStack/src/App.jsx
+++ 0x07-TanStack/src/App.jsx
@@ -1,23 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Order from "./Order";
-import PizzaOfTheDay from "./PizzaOfTheDay";
-import Header from "./Header";
-import { CartContext } from "./contexts";
-import { useState } from "react";
-//import Header from "./Header";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+const router = createRouter({ routeTree });
 
 const App = () => {
-  const carthook = useState([]);
   return (
     <StrictMode>
-      <CartContext.Provider value={carthook}>
-        <div>
-          <Header />
-          <Order />
-          <PizzaOfTheDay />
-        </div>
-      </CartContext.Provider>
+      <RouterProvider router={router} />
     </StrictMode>
   );
 };
