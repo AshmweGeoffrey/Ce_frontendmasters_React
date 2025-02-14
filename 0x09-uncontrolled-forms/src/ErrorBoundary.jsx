f7ef7e5c9ac83386fdcb0bd78ae16ba17875870f
AshmweGeoffrey <ashimwegeoffrey@gmail.com>
2025-02-14 13:42:17 +0200

    Add: Uncontrolled Forms initial

---
+++ 0x09-uncontrolled-forms/src/ErrorBoundary.jsx
@@ -0,0 +1,27 @@
+import { Link } from "@tanstack/react-router";
+import { Component } from "react";
+class ErrorBoundary extends Component {
+    state = {hasError: false}
+    static getDerivedstateFromError() {
+        return {hasError: true }
+    }
+    componentDidCatch(error,info){
+        console.error("ErrorBoundary Caught an Error",error,info)
+    }
+    render (){
+        if (this .state.hasError){
+            return (
+        <div className="error-boundary">
+            <h2>Uh oh!</h2>
+            <p>
+                There was an with this page <Link to="/">click here to go back to the homepage</Link>
+                
+            </p>
+        </div>
+        )
+    }
+    return this.props.children;
+    }
+}
+ErrorBoundary.getDerivedstateFrom()
+export default ErrorBoundary 
